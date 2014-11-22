f785ff550e3de48d80aebea8fa6b57d66cb68dd3
unknown <fad4470@GDD-05.main.ad.rit.edu>
2014-11-22 14:37:40 -0500

    Basic fluid stuffs MkI -Frankie

---
+++ Assets/Particle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particle : MonoBehaviour {
+	public float pull = 5f;
+	
+	private Transform fluidCenter;
+	private Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+		fluidCenter = GameObject.FindWithTag("FluidCenter").transform;
+		rigid = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = fluidCenter.position - this.transform.position;
+		dir.Normalize();
+		this.rigid.AddForce(dir * pull * Time.deltaTime*1000); 
+	}
+}
