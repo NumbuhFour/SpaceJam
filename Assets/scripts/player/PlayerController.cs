9bb4e9a9c4ee9c972f0b16df2aab6ab487f06d43
unknown <fad4470@GDD-05.main.ad.rit.edu>
2014-11-22 13:09:18 -0500

    Begin Testing -Frankie

---
+++ Assets/scripts/player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horiz = Input.GetAxisRaw("Horizontal");
+		float vert = Input.GetAxisRaw("Vertical");
+
+		if(horiz != 0){
+			Vector3 position = this.transform.position;
+			position.x += horiz*speed;
+			this.transform.position = position;
+		}
+		if(vert != 0){
+			Vector3 position = this.transform.position;
+			position.y += vert*speed;
+			this.transform.position = position;
+		}
+	}
+}
