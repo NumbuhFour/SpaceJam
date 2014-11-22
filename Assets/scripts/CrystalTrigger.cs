6e62f1ba64647769a655e0fe59dae9422e508b3b
unknown <fad4470@GDD-05.main.ad.rit.edu>
2014-11-22 16:04:43 -0500

    Added crystal functionality. -Frankie

---
+++ Assets/scripts/CrystalTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter2D(Collider2D coll){
+		GameObject.Destroy(this.gameObject);
+		GameObject.Find("Main").GetComponent<Main>().Score += 100;
+	}
+}
